9284500aeb4cab9e30db0868682dfbc1aee46c46
SHIVAM-SHRIVATSAVA-1605 <shivam091204@gmail.com>
2025-10-27 13:58:42 +0530

    Added v1 hashtag model and tweet model

--- src/index.js
+++ src/index.js
@@ -10,10 +10,4 @@ app.listen(3000, async () => {
     console.log("server is running ");
     await connect();
     console.log("MongoDb connected");
-
-    const tweetRepo = new TweetReposiroty();
-    // const tweet = await tweetRepo.getAll(0,2);
-    // console.log(tweet[0].contentWithEmail);
-    const tweet = await tweetRepo.create({content: 'With the hooks'});
-    console.log(tweet);
 });
